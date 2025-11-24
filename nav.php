498ae645846433a28dd49ddd5daef69fd5be32e7
StenChristjanUesson <StenUesson@gmail.com>
2025-11-24 12:34:08 +0200

    navigatsiooni rippmenü on tehtud

--- nav.php
+++ nav.php
@@ -1,28 +1,42 @@
-<nav>
-    <ul>
-        <li>
-            <a href="?leht=index.php">Kodu</a>
-        </li>
-        <li>
-            <a href="?leht=jsToo.php">JS joonis</a>
-        </li>
-        <li>
-            <a href="?leht=ajafunktsioonid.php">Ajafunktsioonid</a>
-        </li>
-        <li>
-            <a href="?leht=canvas.php">canvas</a>
-        </li>
-        <li>
-            <a href="https://stenchristjanuesson24.thkit.ee" target="_blank">Vana index.html</a>
-        </li>
-        <li>
-            <a href="?leht=gitKasud.php">Git käsud</a>
-        </li>
-        <li>
-            <a href="?leht=tekstfunktsioonid.php">Tekstfunktsioonid</a>
-        </li>
-        <li>
-            <a href="?leht=matemaatilised.php">Matem. Funktsioonid</a>
-        </li>
-    </ul>
-</nav>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navigatsioon</title>
+    <link rel="stylesheet" href="rippMenuStyle.css">
+</head>
+<body>
+    <div id="main">
+        <div id="content">
+            <nav class="menu">
+                <ul>
+                    <li>
+                        <a href="?leht=index.php">Kodu</a>
+                    </li>
+                    <li>
+                        <a href="?leht=jsToo.php">JS joonis</a>
+                    </li>
+                    <li>
+                        <a href="?leht=ajafunktsioonid.php">Ajafunktsioonid</a>
+                    </li>
+                    <li>
+                        <a href="?leht=canvas.php">canvas</a>
+                    </li>
+                    <li>
+                        <a href="https://stenchristjanuesson24.thkit.ee" target="_blank">Vana index.html</a>
+                    </li>
+                    <li>
+                        <a href="?leht=gitKasud.php">Git käsud</a>
+                    </li>
+                    <li>
+                        <a href="?leht=tekstfunktsioonid.php">Tekstfunktsioonid</a>
+                    </li>
+                    <li>
+                        <a href="?leht=matemaatilised.php">Matem. Funktsioonid</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</body>
